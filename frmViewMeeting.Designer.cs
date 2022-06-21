8af77350086afcdbeecc1e094ed505c06939825c
Keagan <Keagandc@agilebridge.co.za>
2022-06-21 18:18:25 +0200

    Working Agenda

--- frmViewMeeting.Designer.cs
+++ frmViewMeeting.Designer.cs
@@ -32,10 +32,10 @@
             this.btnsearch = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.txtSearchAgenda = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.dtpSearchDate = new System.Windows.Forms.DateTimePicker();
             this.label3 = new System.Windows.Forms.Label();
+            this.cbxMeetings = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvMeeting)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,22 +45,22 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvMeeting.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvMeeting.Location = new System.Drawing.Point(22, 120);
-            this.dgvMeeting.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvMeeting.Location = new System.Drawing.Point(29, 148);
+            this.dgvMeeting.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dgvMeeting.Name = "dgvMeeting";
             this.dgvMeeting.RowHeadersWidth = 51;
             this.dgvMeeting.RowTemplate.Height = 24;
-            this.dgvMeeting.Size = new System.Drawing.Size(1074, 572);
+            this.dgvMeeting.Size = new System.Drawing.Size(1432, 704);
             this.dgvMeeting.TabIndex = 0;
             // 
             // btnsearch
             // 
             this.btnsearch.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.btnsearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnsearch.Location = new System.Drawing.Point(697, 56);
-            this.btnsearch.Margin = new System.Windows.Forms.Padding(2);
+            this.btnsearch.Location = new System.Drawing.Point(929, 69);
+            this.btnsearch.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnsearch.Name = "btnsearch";
-            this.btnsearch.Size = new System.Drawing.Size(105, 28);
+            this.btnsearch.Size = new System.Drawing.Size(140, 34);
             this.btnsearch.TabIndex = 1;
             this.btnsearch.Text = "Search";
             this.btnsearch.UseVisualStyleBackColor = true;
@@ -71,8 +71,7 @@
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(432, 9);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(576, 11);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(255, 37);
             this.label1.TabIndex = 3;
@@ -82,30 +81,22 @@
             // 
             this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(806, 56);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Location = new System.Drawing.Point(1075, 69);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(116, 28);
+            this.button1.Size = new System.Drawing.Size(155, 34);
             this.button1.TabIndex = 4;
             this.button1.Text = "Cancel";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // txtSearchAgenda
-            // 
-            this.txtSearchAgenda.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.txtSearchAgenda.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSearchAgenda.Location = new System.Drawing.Point(166, 58);
-            this.txtSearchAgenda.Name = "txtSearchAgenda";
-            this.txtSearchAgenda.Size = new System.Drawing.Size(526, 26);
-            this.txtSearchAgenda.TabIndex = 5;
             // 
             // label2
             // 
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(18, 60);
+            this.label2.Location = new System.Drawing.Point(24, 74);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(144, 20);
             this.label2.TabIndex = 6;
@@ -116,9 +107,10 @@
             this.dtpSearchDate.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.dtpSearchDate.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dtpSearchDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dtpSearchDate.Location = new System.Drawing.Point(166, 89);
+            this.dtpSearchDate.Location = new System.Drawing.Point(221, 110);
+            this.dtpSearchDate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dtpSearchDate.Name = "dtpSearchDate";
-            this.dtpSearchDate.Size = new System.Drawing.Size(526, 26);
+            this.dtpSearchDate.Size = new System.Drawing.Size(700, 26);
             this.dtpSearchDate.TabIndex = 7;
             // 
             // label3
@@ -126,30 +118,42 @@
             this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(18, 89);
+            this.label3.Location = new System.Drawing.Point(24, 110);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(123, 20);
             this.label3.TabIndex = 8;
             this.label3.Text = "Search by Date:";
             // 
+            // cbxMeetings
+            // 
+            this.cbxMeetings.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.cbxMeetings.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxMeetings.FormattingEnabled = true;
+            this.cbxMeetings.Location = new System.Drawing.Point(221, 71);
+            this.cbxMeetings.Name = "cbxMeetings";
+            this.cbxMeetings.Size = new System.Drawing.Size(700, 28);
+            this.cbxMeetings.TabIndex = 9;
+            // 
             // frmViewMeeting
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1133, 803);
+            this.ClientSize = new System.Drawing.Size(1511, 988);
+            this.Controls.Add(this.cbxMeetings);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.dtpSearchDate);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtSearchAgenda);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnsearch);
             this.Controls.Add(this.dgvMeeting);
-            this.Margin = new System.Windows.Forms.Padding(2);
-            this.MinimumSize = new System.Drawing.Size(1128, 842);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MinimumSize = new System.Drawing.Size(1499, 1027);
             this.Name = "frmViewMeeting";
             this.Text = "frmViewMeeting";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmViewMeeting_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvMeeting)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -162,9 +166,9 @@
         private System.Windows.Forms.Button btnsearch;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox txtSearchAgenda;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.DateTimePicker dtpSearchDate;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbxMeetings;
     }
 }
--- frmViewMeeting.cs
+++ frmViewMeeting.cs
@@ -14,7 +14,9 @@ namespace SAIMC_MemberManager
     {
         List<Member> members = new List<Member>();
         List<MemberMeeting> Membermeetings = new List<MemberMeeting>();
-        List<Member> foundMembersInMeeting = new List<Member>();
+        List<MemberMeeting> foundMemberMeetings = new List<MemberMeeting>();
+        
+        List<Meeting> MeetingList = new List<Meeting>();
         SAIMCEntities db = new SAIMCEntities();
         
         public frmViewMeeting()
@@ -24,31 +26,43 @@ namespace SAIMC_MemberManager
 
         private void btnsearch_Click(object sender, EventArgs e)
         {
-
+            List<Member> foundMembersInMeeting = new List<Member>();
+            //Search by Agenda
+            MeetingList = db.Meetings.ToList();
+            var foundMeeting =  MeetingList.Find(x => x.Agenda == cbxMeetings.Text); 
             Membermeetings = db.MemberMeetings.ToList();
+            foundMemberMeetings = Membermeetings.FindAll(x => x.Meetingid == foundMeeting.Meetingid);        
+
             members = db.Members.ToList();
-            var FoundMemberMeeting = Membermeetings.FindAll(x => x.Meetingid == 132).ToList();
-            foreach(var membermeeting in FoundMemberMeeting)
+            
+            //Loop through MemberMeeting List to Find all Members ID's and Add Member Details to List
+            foreach (var membermeeting in foundMemberMeetings)
             {                
                 foreach(var member in members)
                 {
                     if(member.id == membermeeting.id)
                     {
-                        foundMembersInMeeting.Add(member);
-                        
+                        foundMembersInMeeting.Add(member);                        
                     }
-                }               
-
+                }  
             }
             dgvMeeting.DataSource = foundMembersInMeeting;
-
             dgvMeeting.Update();
-            dgvMeeting.Refresh();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void frmViewMeeting_Load(object sender, EventArgs e)
+        {
+            MeetingList = db.Meetings.ToList();
+            foreach(var meeting in MeetingList)
+            {
+                cbxMeetings.Items.Add(meeting.Agenda);
+            }
+            
         }
     }
 }
