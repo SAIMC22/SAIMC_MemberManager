b6b8a642aab0c7142478134882489527de1a108a
Keagan <Keagandc@agilebridge.co.za>
2022-06-27 17:30:35 +0200

    Editing Member

--- frmEditMember.Designer.cs
+++ frmEditMember.Designer.cs
@@ -69,7 +69,7 @@
             // 
             this.nudCellNumber.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.nudCellNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nudCellNumber.Location = new System.Drawing.Point(419, 278);
+            this.nudCellNumber.Location = new System.Drawing.Point(410, 312);
             this.nudCellNumber.Maximum = new decimal(new int[] {
             1215752191,
             23,
@@ -83,7 +83,7 @@
             // 
             this.nudIdNumber.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.nudIdNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nudIdNumber.Location = new System.Drawing.Point(419, 328);
+            this.nudIdNumber.Location = new System.Drawing.Point(410, 362);
             this.nudIdNumber.Maximum = new decimal(new int[] {
             1569325055,
             23283064,
@@ -97,7 +97,7 @@
             // 
             this.nudMembershipNumber.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.nudMembershipNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nudMembershipNumber.Location = new System.Drawing.Point(419, 124);
+            this.nudMembershipNumber.Location = new System.Drawing.Point(410, 158);
             this.nudMembershipNumber.Maximum = new decimal(new int[] {
             -559939585,
             902409669,
@@ -112,7 +112,7 @@
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(256, 474);
+            this.label2.Location = new System.Drawing.Point(247, 508);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(75, 20);
@@ -124,7 +124,7 @@
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(264, 418);
+            this.label1.Location = new System.Drawing.Point(255, 452);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(67, 20);
@@ -139,7 +139,7 @@
             this.cbxpayment.Items.AddRange(new object[] {
             "Paid",
             "Unpaid"});
-            this.cbxpayment.Location = new System.Drawing.Point(419, 469);
+            this.cbxpayment.Location = new System.Drawing.Point(410, 503);
             this.cbxpayment.Margin = new System.Windows.Forms.Padding(4);
             this.cbxpayment.Name = "cbxpayment";
             this.cbxpayment.Size = new System.Drawing.Size(336, 28);
@@ -154,7 +154,7 @@
             "Male",
             "Female",
             "Other"});
-            this.cbxgender.Location = new System.Drawing.Point(419, 416);
+            this.cbxgender.Location = new System.Drawing.Point(410, 450);
             this.cbxgender.Margin = new System.Windows.Forms.Padding(4);
             this.cbxgender.Name = "cbxgender";
             this.cbxgender.Size = new System.Drawing.Size(336, 28);
@@ -165,7 +165,7 @@
             this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(192, 123);
+            this.label3.Location = new System.Drawing.Point(183, 157);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(160, 20);
@@ -177,7 +177,7 @@
             this.label5.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(228, 374);
+            this.label5.Location = new System.Drawing.Point(219, 408);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(103, 20);
@@ -189,7 +189,7 @@
             this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(242, 330);
+            this.label4.Location = new System.Drawing.Point(233, 364);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(90, 20);
@@ -201,7 +201,7 @@
             this.label6.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(233, 278);
+            this.label6.Location = new System.Drawing.Point(224, 312);
             this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(99, 20);
@@ -212,7 +212,7 @@
             // 
             this.dob.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.dob.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dob.Location = new System.Drawing.Point(419, 372);
+            this.dob.Location = new System.Drawing.Point(410, 406);
             this.dob.Margin = new System.Windows.Forms.Padding(4);
             this.dob.Name = "dob";
             this.dob.Size = new System.Drawing.Size(336, 26);
@@ -222,7 +222,7 @@
             // 
             this.txtSurname.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.txtSurname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSurname.Location = new System.Drawing.Point(419, 226);
+            this.txtSurname.Location = new System.Drawing.Point(410, 260);
             this.txtSurname.Margin = new System.Windows.Forms.Padding(4);
             this.txtSurname.Name = "txtSurname";
             this.txtSurname.Size = new System.Drawing.Size(336, 26);
@@ -232,7 +232,7 @@
             // 
             this.txtName.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtName.Location = new System.Drawing.Point(419, 172);
+            this.txtName.Location = new System.Drawing.Point(410, 206);
             this.txtName.Margin = new System.Windows.Forms.Padding(4);
             this.txtName.MinimumSize = new System.Drawing.Size(4, 20);
             this.txtName.Name = "txtName";
@@ -244,7 +244,7 @@
             this.label7.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(253, 228);
+            this.label7.Location = new System.Drawing.Point(244, 262);
             this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(78, 20);
@@ -256,7 +256,7 @@
             this.label11.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(276, 174);
+            this.label11.Location = new System.Drawing.Point(267, 208);
             this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(55, 20);
@@ -267,7 +267,7 @@
             // 
             this.btncancel.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.btncancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btncancel.Location = new System.Drawing.Point(420, 561);
+            this.btncancel.Location = new System.Drawing.Point(411, 595);
             this.btncancel.Margin = new System.Windows.Forms.Padding(4);
             this.btncancel.Name = "btncancel";
             this.btncancel.Size = new System.Drawing.Size(337, 34);
@@ -280,7 +280,7 @@
             // 
             this.btnSaveMem.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.btnSaveMem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSaveMem.Location = new System.Drawing.Point(420, 519);
+            this.btnSaveMem.Location = new System.Drawing.Point(411, 553);
             this.btnSaveMem.Margin = new System.Windows.Forms.Padding(4);
             this.btnSaveMem.Name = "btnSaveMem";
             this.btnSaveMem.Size = new System.Drawing.Size(337, 34);
@@ -294,7 +294,7 @@
             this.label9.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(456, 35);
+            this.label9.Location = new System.Drawing.Point(447, 69);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(214, 39);
             this.label9.TabIndex = 101;
@@ -304,7 +304,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1038, 611);
+            this.ClientSize = new System.Drawing.Size(1038, 660);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.nudCellNumber);
--- frmEditMember.cs
+++ frmEditMember.cs
@@ -61,7 +61,71 @@ namespace SAIMC_MemberManager
 
         private void btnSaveMem_Click(object sender, EventArgs e)
         {
+            Member mymembers = new Member();
+            try
+            {
+                //Add Member Details to system and Create a QR Code for that Member
+                if (txtName.Text != "" && txtSurname.Text != "" && nudCellNumber.Value != 0 && nudIdNumber.Value != 0 && cbxgender.Text != "" && cbxpayment.Text != "" && nudMembershipNumber.Value != 0)
+                {
+                    if (nudIdNumber.Value.ToString().Length != 13)
+                    {
+                        MessageBox.Show("Invalid Id Number");
+                        return;
+                    }
+                    if (nudCellNumber.Value.ToString().Length != 10)
+                    {
+                        MessageBox.Show("Invalid Cell Number");
+                        return;
+                    }
+                    //Save New Member to Database
+                    mymembers.MemberShipNo = Convert.ToInt32(nudMembershipNumber.Value);
+                    mymembers.Name = txtName.Text;
+                    mymembers.Surname = txtSurname.Text;
+                    mymembers.ContactNumber = nudCellNumber.Value.ToString();
+                    mymembers.IdNumber = nudIdNumber.Value.ToString();
+                    mymembers.DOB = dob.Value.Date;
+                    mymembers.Gender = cbxgender.Text;
 
+                    if (cbxpayment.Text == "Paid")
+                    {
+                        mymembers.Haspaid = true;
+
+                    }
+                    if (cbxpayment.Text == "unpaid")
+                    {
+                        mymembers.Haspaid = false;
+
+                    }
+                    db.Members.Add(mymembers);
+
+                    string message = "Please confirm creation of:" + txtName.Text + " " + txtSurname.Text; ;
+                    string title = "Please Confirm";
+                    MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                    DialogResult result = MessageBox.Show(message, title, buttons);
+                    if (result == DialogResult.No)
+                    {
+
+                    }
+                    else
+                    {
+                        db.SaveChanges();
+                        MessageBox.Show("New Member Successfully Created");
+                        frmManageMembers frmManageMembers = new frmManageMembers();
+                        frmManageMembers.ShowDialog();
+                        this.Close();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please Fill in All required Fields");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Creation Failed,Please try Again");
+            }
+
+}
         }
     }
-}
+
